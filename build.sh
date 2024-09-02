a11d400eab23fb502a58813c39a8337889d5871a
zee-ham-su <hsufiian@yahoo.com>
2024-09-02 11:16:14 +0000

    chore: Update Django and djangorestframework versions in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+#!/bin/bash
 python -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install Django==4.2 
-pip install djangorestframework 
-pip install djongo
+pip install Django>=4.2 djangorestframework==3.15.2 djongo==1.3.6
