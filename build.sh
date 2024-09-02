318d0d830d7f5124369577b958ffd8d773d09d88
zee-ham-su <hsufiian@yahoo.com>
2024-09-02 11:09:29 +0000

    chore: Update Django and djangorestframework versions in requirements.txt

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 python -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install Django==4.1.x djangorestframework djongo
+pip install Django==4.1.13 
+pip install djangorestframework 
+pip install djongo
